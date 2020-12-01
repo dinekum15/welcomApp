e131f5d31249fd92f962ffbcce458ce3bd435a1d
dinekum15 <dinekum15@gmail.com>
2020-12-01 11:11:51 +0530

    linked to firebase database

--- welcomApp/main.py
+++ welcomApp/main.py
@@ -4,54 +4,60 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 from kivy.core.window import Window
 
+import json
+import requests
+
 Window.size = (340, 600)
 
-class LoginWindow(Screen):
-    userName = ObjectProperty(None)
-    lastName = ObjectProperty(None)
-    hidden = ObjectProperty(None)
-    def SubmitBtn(self):
-        if (self.userName != '') & (self.lastName.text != ''):
-            MainWindow.a = self.userName.text
-            MainWindow.b = self.lastName.text
-            wm.current = 'main'
-            self.reset()
-        else:
-            self.hidden.text = 'no input'
+class LoginScreen(Screen):
+	pass
 
-    def reset(self):
-        self.userName.text = ""
-        self.lastName.text = ""
-        self.hidden.text = ""
+class MainScreen(Screen):
+	pass
 
-
-class MainWindow(Screen):
-    fName = ObjectProperty(None)
-    lName = ObjectProperty(None)
-    a = ''
-    b = ''
-
-    def on_enter(self, *args):
-        self.fName.text = self.a
-        self.lName.text = self.b
-
-
-class WindowManager(ScreenManager):
-    pass
-
-kv = Builder.load_file('welcom.kv')
-
-wm = WindowManager()
-
-screens = [LoginWindow(name = 'login'), MainWindow(name = 'main')]
-for screen in screens:
-    wm.add_widget(screen)
-
-wm.current = 'login'
 
 class welcomApp(App):
-    def build(self):
-        return wm
+	auth_key = 'PYLzTEO8YhZacPGceFwJe56pbRTazFZdDERAzP6I'
+	url = 'https://screendatabase-908ba.firebaseio.com/Users'
+		
+	def build(self):
+		kv = Builder.load_file('welcom.kv')
+		return kv
+	def login(self):
+		#print(self.root.ids.login_screen.ids.staffno.text)
+		uid = self.root.ids.login_screen.ids.staffno.text
+		pwd = self.root.ids.login_screen.ids.pwd.text
+		self.logincheck = False
+		if uid!='' and pwd!='':
+			if self.validateLogin(uid, pwd):
+				self.logincheck = True
+				self.userInfo = self.GetFromDB(uid)
+				print(self.userInfo)
+				self.welcom()
+				self.root.ids.login_screen.ids.staffno.text = ''
+				self.root.ids.login_screen.ids.pwd.text = ''
+				self.root.ids.login_screen.ids.loginerror.text = ''
+			else:
+				self.root.ids.login_screen.ids.loginerror.text = 'Incorrect staff no or password'
+		else:
+			self.root.ids.login_screen.ids.loginerror.text = 'Enter your login details'
+	
+	def welcom(self):
+		self.root.current = 'main'
+		if self.logincheck:
+			self.root.ids.main_screen.ids.uName.text = self.userInfo['Name']
+			self.root.ids.main_screen.ids.uStaffNo.text = self.userInfo['Staff_No']
+	
+	def validateLogin(self, *args):
+		url = f'{self.url}/{args[0]}/.json'
+		data = requests.get(url+'?auth='+self.auth_key).json()
+		if args[0] == data['Staff_No'] and args[1] == data['Password']:
+			return True
+		else:
+			return False
+	def GetFromDB(self,*args):
+		url = f'{self.url}/{args[0]}/.json'
+		data = requests.get(url+'?auth='+self.auth_key).json()
+		return data
 
-if __name__ == '__main__':
-    welcomApp().run()
+welcomApp().run()
